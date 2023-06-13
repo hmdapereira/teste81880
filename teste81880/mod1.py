468a6fc6d9d0df0d977a8a9591687b282ca52f7d
hmdapereira <hernanimapereira@gmail.com>
2023-06-13 12:56:16 +0100

    First Commit

---
+++ teste81880/mod1.py
@@ -0,0 +1,5 @@
+def maxlen(word1, word2):
+    return max(len(word1), len(word2))
+
+def minstring(word1, word2):
+    return min(len(word1), len(word2))
---
+++ teste81880/mod2.py
@@ -0,0 +1,5 @@
+from mod1 import maxlen
+
+def meanString(word1, word2):
+    total_length = len(word1) + len(word2)
+    return total_length / 2
